7abad3948feabeeca0eb078c22c060d590f77dcf
akine <weezer.and.you@gmail.com>
2020-11-27 18:44:01 +0900

    add: CommentIndexView

--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Comment
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the comments index.")
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the comments index.")
+
+class CommentIndexView(ListView):
+    model = Comment
